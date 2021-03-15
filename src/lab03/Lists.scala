3cca8de85b92edea485161e830109504211e5bc2
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-03-15 11:51:45 +0100

    Part1b: flatMap

--- src/lab03/Lists.scala
+++ src/lab03/Lists.scala
@@ -1,39 +1,19 @@
 package lab03
 
+import u03.Lists.List._
+
 object Lists {
+  import u03.Lists._
 
-  sealed trait List[E]
-  object List {
-    case class Cons[E](head: E, tail: List[E]) extends List[E]
-    case class Nil[E]() extends List[E]
+  def drop[A](list: List[A], n: Int): List[A] = list match {
+    case Cons(_, t) if n > 0 => drop(t, n-1)
+    case Cons(h, t) if n <= 0 => Cons(h, t)
+    case _ => Nil()
+  }
 
-    def sum(l: List[Int]): Int = l match {
-      case Cons(h, t) => h + sum(t)
-      case _ => 0
-    }
-
-    def append[A](l1: List[A], l2: List[A]): List[A] = (l1, l2) match {
-      case (Cons(h, t), l2) => Cons(h, append(t, l2))
-      case _ => l2
-    }
-
-    def map[A,B](l: List[A])(mapper: A=>B): List[B] = l match {
-      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
-      case Nil() => Nil()
-    }
-
-    def filter[A](l1: List[A])(pred: A=>Boolean): List[A] = l1 match {
-      case Cons(h,t) if (pred(h)) => Cons(h, filter(t)(pred))
-      case Cons(_,t) => filter(t)(pred)
-      case Nil() => Nil()
-    }
-
-    def drop[A](list: List[A], n: Int): List[A] = list match {
-      case Cons(_, t) if n > 0 => drop(t, n-1)
-      case Cons(h, t) if n <= 0 => Cons(h, t)
-      case Nil() => Nil()
-    }
-
+  def flatMap[A,B](list: List[A])(mapper: A => List[B]): List[B] = list match {
+    case Cons(h, t) => append(mapper(h), flatMap(t)(mapper))
+    case _ => Nil()
   }
 
 }
--- test/lab03/ListsTest.scala
+++ test/lab03/ListsTest.scala
@@ -1,17 +1,31 @@
 package lab03
 
-import lab03.Lists.List._
+import lab03.Lists._
+import u03.Lists.List._
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class ListsTest {
 
+  val lst = Cons(10, Cons(20, Cons(30, Nil())))
+
   @Test
   def dropTest(): Unit = {
-    val lst = Cons(10, Cons(20, Cons(30, Nil())))
     assertEquals(Cons(20, Cons(30, Nil())), drop(lst, 1))
     assertEquals(Cons(30, Nil()), drop(lst, 2))
     assertEquals(Nil(), drop(lst, 5))
+  }
+
+  @Test
+  def flatMapTest(): Unit = {
+    assertEquals(
+      Cons(11,Cons(21,Cons(31,Nil()))),
+      flatMap(lst)(v => Cons(v+1, Nil()))
+    )
+    assertEquals(
+      Cons(11,Cons(12,Cons(21,Cons(22,Cons(31,Cons(32,Nil())))))),
+      flatMap(lst)(v => Cons(v+1, Cons(v+2, Nil())))
+    )
   }
 
 }
